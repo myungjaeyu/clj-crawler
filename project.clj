1486bcf22d6216da4250972cfa8a5ef5a8dc10ad
myungjaeyu <rootcode10@gmail.com>
2019-01-04 07:09:36 +0900

    Added clj-http to project.clj

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject crawler "0.1.0-crawler"
   :description "0.1.0-crawler"
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [clj-http "3.9.1"]]
   :main crawler.core)
