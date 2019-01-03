44a7c62d90cdc92d9f00469353865f352ebfd13e
myungjaeyu <rootcode10@gmail.com>
2019-01-04 07:11:22 +0900

    Added enlive to project.clj

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject crawler "0.1.0-crawler"
   :description "0.1.0-crawler"
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [clj-http "3.9.1"]]
+                 [clj-http "3.9.1"]
+                 [enlive "1.1.6"]]
   :main crawler.core)
